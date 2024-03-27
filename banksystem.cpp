e8b8a7c7d7272a00d366f29521bd0cc87fad3fcb
neozmmv <83484706+neozmmv@users.noreply.github.com>
2024-03-27 15:52:29 -0300

    Add files via upload

---
+++ banksystem.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+class BankAccount
+{
+    private:
+    float amount;
+    public:
+    float balance, deposit_amount, withdraw_amount;
+    float add()
+    {
+        cout << "Insert deposit amount: ";
+        cin >> amount;
+        deposit_amount = balance + amount;
+        cout << "Amount deposited: " << amount << ". Current balance: " << deposit_amount << endl;
+        return deposit_amount;
+    }
+    float withdraw()
+    {
+        cout << "Insert withdraw amount: ";
+        cin >> amount;
+        withdraw_amount = balance - amount;
+        cout << "Amount withdrawn: " << amount << ". Current balance: " << withdraw_amount << endl;
+        return withdraw_amount;
+    }
+};
+
+int main()
+{
+    BankAccount conta1;
+    fstream file;
+    file.open("conta1.txt", ios::in);
+    if(file.is_open())
+    {
+        string line;
+        while(getline(file, line))
+        {
+            conta1.balance = stof(line);
+            cout << line << endl;
+        }
+    }
+    file.close();
+
+    cout << "\n1 - Withdraw\n2 - Deposit\n3 - Balance\n4 - Exit\n\n";
+    char operation;
+    cin >> operation;
+
+    if(operation == '1')
+    {
+        file.open("conta1.txt", ios:: out);
+        if(file.is_open())
+        {
+            file << conta1.withdraw() << endl;
+        }
+        file.close();
+    }
+
+    else if(operation == '2')
+    {
+        file.open("conta1.txt", ios::out);
+        if(file.is_open())
+        {
+            file << conta1.add() << endl;
+        }
+        file.close();
+    }
+
+    else if(operation == '3')
+    {
+        cout << "Current balance is: " << conta1.balance << endl;
+        return 1;
+    }
+    
+    else if(operation == '4')
+    {
+        cout << "Exiting!\n" << endl;
+        return 1;
+    }
+    else
+    {
+        cout << "Invalid operation!\n";
+        return 2;
+    }
+}
